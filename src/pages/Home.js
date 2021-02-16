5137bc2fc1288ac3ffc3cb6487d5bd1df9a45934
Jagge <jsandberg_8@hotmail.com>
2021-02-16 13:25:17 +0100

    Created a Home Comp

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import style from "../css/Home.module.css"
+
+function Home  () {
+  return ( 
+    <div className={style.home}>
+      <div className={style.wrapper}>
+        <h1>The calculator will be here</h1>
+      </div>
+    </div>
+   );
+}
+ 
+export default Home;
