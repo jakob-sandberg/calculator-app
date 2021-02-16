1cc6857a9da76d2676de198fd2f9a2f5490c8c5e
Jagge <jsandberg_8@hotmail.com>
2021-02-16 15:56:19 +0100

    CAlc done

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,14 @@
 import { displayWrapper, display, input } from "../css/Display.module.css"
+import { useContext } from "react"
+import { DataContext} from "../context/DataContext"
 
 function Display ()  {
+
+  const { dataInput } = useContext(DataContext)
   return ( 
     <div className={displayWrapper}>
       <div className={display}>
-        <span className={input}>Here is the data</span>
+        <span className={input}>{dataInput}</span>
       </div>
     </div>
    );
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,22 +1,59 @@
-import { createContext, useState } from "react"
+import { createContext, useState } from "react";
 
 export const DataContext = createContext();
 
-function DataProvider (props) {
+function DataProvider(props) {
   const [dataInput, setDataInput] = useState("");
 
   const handleClick = (value) => {
-    console.log("button pressed: ", value);
-  }
+    console.log("Button pressed: ", value);
+    if (/\d/.test(value)) {
+      setDataInput(dataInput + value);
+    } else if (value === "clear") {
+      setDataInput("");
+    } else if (value === "=") {
+      console.log("Calculating...");
+      calculate(dataInput);
+    } else {
+      setDataInput(dataInput + ` ${value} `);
+    }
+  };
+
+  const calculate = (calculation) => {
+    console.log(dataInput);
+    let calcArray = calculation.split(" ");
+    let operatorsArray = calcArray.filter((e) => /\D/.test(e)); // Båda dessa filter kommer inte hantera decimaltal.
+    let numbersArray = calcArray.filter((e) => !/\D/.test(e));
+    console.log(operatorsArray);
+    console.log(numbersArray);
+    let result = parseInt(numbersArray[0]);
+    for (let i = 0; i < numbersArray.length; i++) {
+      switch (operatorsArray[i]) {
+        case "+":
+          result += parseInt(numbersArray[i + 1]);
+          break;
+        case "-":
+          result -= numbersArray[i + 1];
+          break;
+        case "x":
+          result *= numbersArray[i + 1];
+          break;
+        case "/":
+          result /= numbersArray[i + 1];
+          break;
+      }
+    }
+    setDataInput(result);
+  };
+
   const values = {
     handleClick,
-  }
-  
-    return(
-    <DataContext.Provider value={values}>
-      {props.children}
-    </DataContext.Provider>
-  )
+    dataInput,
+  };
+
+  return (
+    <DataContext.Provider value={values}>{props.children}</DataContext.Provider>
+  );
 }
 
 export default DataProvider;
