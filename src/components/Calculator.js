7b7af42b28ecdb855ec19f9e316105c39a47a99f
Jagge <jsandberg_8@hotmail.com>
2021-02-16 13:41:17 +0100

    Created a Calc comp

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import { calculator } from "../css/Calculator.module.css"
+
+function Calculator () {
+  return ( 
+    <div className={calculator}>
+      
+    </div>
+   );
+}
+ 
+export default Calculator;
