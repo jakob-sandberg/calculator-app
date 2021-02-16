4bccafd7f890730be630f20c130fd2d1a62d2e13
Jagge <jsandberg_8@hotmail.com>
2021-02-16 14:21:17 +0100

    Created display and import display in calc

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,11 @@
 import { calculator } from "../css/Calculator.module.css"
+import Display from "./Display";
 
 function Calculator () {
   return ( 
     <div className={calculator}>
-      <h1>This is Calc comp</h1>
+      <Display />
+      <h2>This is Button comp</h2>
     </div>
    );
 }
